00bc781e11dda9041a87639ecb9f375eab38ddaf
Jin Foong <jfhor1991@hotmail.com>
2017-11-15 17:06:31 +0800

    Added logging function

--- mirror-copy.bat
+++ mirror-copy.bat
@@ -2,19 +2,25 @@
 
 SET _SOURCE=%1
 SET _DEST=%2
+SET _LOGFILE=%3
 
 IF "%_SOURCE%" == "" (
 	ECHO Please specify source directory. 
 	ECHO Usage: mirror-copy.bat source destination. 
-	ECHO Exiting with -1...
+	ECHO Exiting with -1 ...
 	EXIT /b -1
 )
 IF "%_DEST%" == "" (
 	ECHO Please specify destination directory. 
 	ECHO Usage: mirror-copy.bat source destination. 
-	ECHO Exiting with -1...
+	ECHO Exiting with -1 ...
 	EXIT /b -1
 )
+IF "%_LOGFILE%" == "" (
+	SET _LOGFILE=%CD%\mirror-copy.log
+)
+
+ECHO Log File : [%_LOGFILE%]
 
 SET _WHAT=/COPY:DAT /MIR 
 :: /COPYALL :: COPY ALL file info
@@ -22,7 +28,7 @@ SET _WHAT=/COPY:DAT /MIR
 :: /MIR :: MIRror a directory tree - equivalent to /PURGE plus all subfolders (/E)
 :: /IS : Include Same, overwrite files even if they are already the same.
 
-SET _OPTIONS=/R:3 /W:5 /NFL /NDL /NP 
+SET _OPTIONS=/R:3 /W:5 /LOG+:%_LOGFILE% /NFL /NDL /NP 
 :: /R:n :: number of Retries
 :: /W:n :: Wait time between retries (seconds)
 :: /LOG+:file : Output status to LOG file (append to existing log).
@@ -30,19 +36,22 @@ SET _OPTIONS=/R:3 /W:5 /NFL /NDL /NP
 :: /NDL :: No dir logging
 :: /NP :: No Progress - do not display % copied.
 
-ECHO %date% %time% ---------- BEGIN ----------
-ECHO Copying from %_SOURCE% to %_DEST% ...
+ECHO %date% %time% -------------------- BEGIN -------------------- >> %_LOGFILE%
+ECHO Copying from [%_SOURCE%] to [%_DEST%] ... >> %_LOGFILE%
 
 IF EXIST "%_SOURCE%" (
-	ROBOCOPY %_SOURCE% %_DEST% %_WHAT% %_OPTIONS%
+	ROBOCOPY %_SOURCE% %_DEST% %_WHAT% %_OPTIONS% > nul 2>&1
 	:: Any value greater than 7 indicates that there was at least one failure during the copy operation.
-	IF %errorlevel% GTR 7 EXIT /b -1
+	IF %errorlevel% GTR 7 (
+		ECHO Error found. Exiting with -1 ... >> %_LOGFILE%
+		EXIT /b -1
+	)
 ) ELSE (
-	ECHO Source directory not found. Copy nothing.
+	ECHO Source directory not found. Copy nothing. >> %_LOGFILE%
 )
 
-ECHO %date% %time% ---------- END ----------
-
+ECHO %date% %time% -------------------- END -------------------- >> %_LOGFILE%
+ECHO. >> %_LOGFILE%
 :: EXIT /b %errorlevel%
-:: return 0 to Tiger to indicates success since ROBOCOPY
+:: return 0 to indicates success since ROBOCOPY
 EXIT /b 0
